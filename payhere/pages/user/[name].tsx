59abf76f1fd52af6635f80055371ca784e94c161
junh0328 <junh0328@naver.com>
2021-11-18 22:26:55 +0900

    feat: axios 모듈 사용하여 github API 요청 및 확인

--- payhere/pages/user/[name].tsx
+++ payhere/pages/user/[name].tsx
@@ -1,12 +1,39 @@
+import axios from 'axios'
 import { useRouter } from 'next/router'
-import React from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 
 const User = () => {
   const router = useRouter()
   const { name } = router.query
+  const [githubName, setGithubName] = useState<string>('')
+
+  useEffect(() => {
+    console.log('githubName:', githubName)
+  }, [githubName])
+
+  useEffect(() => {
+    if (name) {
+      fetchUser(name)
+    }
+  }, [name])
+
+  const fetchUser = useCallback(async (name) => {
+    try {
+      await axios.get(`https://api.github.com/users/${name}`).then((res) => {
+        if (res.status === 200) {
+          console.log('user name:', res.data)
+          setGithubName(res.data.name)
+        }
+      })
+      // await axios.get(`https://api.github.com/users/${name}/repos`).then((res) => console.log(res))
+    } catch (err) {
+      console.error(err)
+    }
+  }, [])
+
   return (
     <div>
-      <h1>{name}님 어서오세요!</h1>
+      <div>{githubName ? <span>{githubName}님 환영합니다</span> : <span>Loading...</span>}</div>
     </div>
   )
 }
